90cfab284f9652c854c58f86a4b553c41b3cd125
tadman <github@tadman.ca>
2009-04-22 13:50:35 -0400

    Add system info command

--- lib/ec2x/bundle/client.rb
+++ lib/ec2x/bundle/client.rb
@@ -4,6 +4,14 @@ class Ec2x::Bundle::Client < Ec2x::Bundle
   # == Class Methods ========================================================
 
   # == Instance Methods =====================================================
+  
+  declare_command :system_info
+  def system_info
+    system_info = Ec2x::Support::SystemInfo.new(@config)
+    
+    puts "IP Address: #{system_info.ip_address}"
+    puts "MAC Address: #{system_info.mac_address}"
+  end
 
   declare_command :connection_status
   
--- lib/ec2x/support/system_info.rb
+++ lib/ec2x/support/system_info.rb
@@ -18,26 +18,27 @@ class Ec2x::Support::SystemInfo
   end
   
   def ip_address
-    IO.popen("#{@config[:ifconfig_path]}") do |fh|
-      fh.readlines.inject([ ]) do |ips, line|
-        line.scan(/inet (?:addr:)?(\d+\.\d+\.\d+\.\d+)/).each do |m|
-          ips << m[0] unless (m[0] == '127.0.0.1')
-        end
-        
-        ips
-      end.first
-    end
+    ifconfig_details.inject([ ]) do |ips, line|
+      line.scan(/inet (?:addr:)?(\d+\.\d+\.\d+\.\d+)/).each do |m|
+        ips << m[0] unless (m[0] == '127.0.0.1')
+      end
+      
+      ips
+    end.first
   end
   
   def mac_address
-    IO.popen("#{@config[:ifconfig_path]}") do |fh|
-      fh.readlines.inject([ ]) do |mac_addrs, line|
-        line.scan(/(?:HWaddr|ether) ((?:[a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2})/).each do |m|
-          mac_addrs << m[0]
-        end
-        
-        mac_addrs
-      end.first
-    end
+    ifconfig_details.inject([ ]) do |mac_addrs, line|
+      line.scan(/(?:HWaddr|ether) ((?:[a-fA-F0-9]{2}:){5}[a-fA-F0-9]{2})/).each do |m|
+        mac_addrs << m[0]
+      end
+      
+      mac_addrs
+    end.first
+  end
+  
+protected
+  def ifconfig_details
+    @ifconfig_details ||= IO.popen("#{@config[:ifconfig_path]}").readlines
   end
 end
